01410413eace89221edb035d62448449ae5a43c6
said <saidnurmatov@yahoo.com>
2024-09-06 09:54:20 +0300

    06.09

---
+++ UserMicroService/src/main/java/com/devsaid/UserMicroService.java
@@ -0,0 +1,11 @@
+package com.devsaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMicroService {
+    public static void main(String[] args) {
+        SpringApplication.run (UserMicroService.class);
+    }
+}
